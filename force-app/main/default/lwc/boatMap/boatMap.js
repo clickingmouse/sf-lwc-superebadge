eb770063665a58e61e7292bbf02967fa74d68bf7
clickingmouse <clickingmouse@hotmail.com>
2020-09-29 11:26:14 +0800

    boatMap done..maybe?

--- force-app/main/default/lwc/boatMap/boatMap.js
+++ force-app/main/default/lwc/boatMap/boatMap.js
@@ -22,13 +22,15 @@ const BOAT_FIELDS = [LONGITUDE_FIELD, LATITUDE_FIELD];
 export default class BoatMap extends LightningElement {
   // private
   subscription = null;
-  boatId;
-
+  @api boatId;
+  //@api recordId;
   // Getter and Setter to allow for logic to run on recordId change
   // this getter must be public
+  @api
   get recordId() {
     return this.boatId;
   }
+
   set recordId(value) {
     this.setAttribute("boatId", value);
     this.boatId = value;
@@ -38,9 +40,12 @@ export default class BoatMap extends LightningElement {
   mapMarkers = [];
 
   // Initialize messageContext for Message Service
+  @wire(MessageContext)
+  messageContext;
 
   // Getting record's location to construct map markers using recordId
   // Wire the getRecord method using ('$boatId')
+  //@wire(getRecord, { recordId: "$boatId", fields: BOAT_FIELDS })
   wiredRecord({ error, data }) {
     // Error handling
     if (data) {
@@ -56,17 +61,31 @@ export default class BoatMap extends LightningElement {
   }
 
   // Runs when component is connected, subscribes to BoatMC
+  subscribeMC() {
+    // Subscribe to the message channel to retrieve the recordID and assign it to boatId.
+    this.subscription = subscribe(
+      this.messageContext,
+      BOATMC,
+      (message) => {
+        //this.recordId(message.boatId);
+        this.boatId = message.recordId;
+      },
+      { scope: APPLICATION_SCOPE }
+    );
+  }
   connectedCallback() {
     // recordId is populated on Record Pages, and this component
-    // should not update when this component is on a record page.
     if (this.subscription || this.recordId) {
       return;
     }
-    // Subscribe to the message channel to retrieve the recordID and assign it to boatId.
+    // should not update when this component is on a record page.
+    this.subscribeMC();
   }
 
   // Creates the map markers array with the current boat's location for the map.
-  updateMap(Longitude, Latitude) {}
+  updateMap(Longitude, Latitude) {
+    return this.mapMarkers.push(Longitude, Latitude);
+  }
 
   // Getter method for displaying the map component, or a helper method.
   get showMap() {
